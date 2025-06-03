9bc549d0273c3eebcbf47b98360cabce57ce7777
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-06-02 21:03:24 -0300

    primeiro commit com [page.replacement.h] header

---
+++ src/page_replacement.h
@@ -0,0 +1,28 @@
+#ifndef PAGE_REPLACEMENT_H
+#define PAGE_REPLACEMENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_PAGES 10
+#define MAX_REFERENCES 100
+
+typedef struct
+{
+    int page_faults;
+    int num_frames;
+    int final_pages[MAX_PAGES];
+} SimulationResult;
+
+// Protótipos das funções
+SimulationResult simulate_fifo(int references[], int num_references, int num_frames);
+SimulationResult simulate_lru(int references[], int num_refetenees, int num_frames);
+
+// Funções Auxiliares
+
+void print_pages_state(int pages[], int num_frames, int step);
+void print_simulation_summary(SimulationResult result, const char *algorithm_name);
+void get_references(int references[], int *num_references);
+void get_num_frames(void);
+void show_menu(void);
