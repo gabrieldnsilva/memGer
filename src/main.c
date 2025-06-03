c168b3b1378ea708c80895159a499290a3dd66be
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-06-02 21:22:17 -0300

    feat: main.c implemented

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "page_replacement.h"
+
+int main(void)
+{
+    int choice;
+    int references[MAX_REFERENCES];
+    int num_references;
+    int num_frames;
+    SimulationResult result_fifo, result_lru;
+}
